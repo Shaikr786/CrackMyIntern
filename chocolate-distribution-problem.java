5cb857327187d64613f5dbfee14ffca9220e06e7
Reshma Shaik <reshma786jrrs@gmail.com>
2024-07-24 23:30:44 +0530

    Day-7

---
+++ chocolate-distribution-problem.java
@@ -0,0 +1,30 @@
+class Solution
+{
+     public long findMinDiff(ArrayList<Integer> a, int n, int m) {
+        // If there are no students or no packets
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+
+        // Sort the given packets
+        Collections.sort(a);
+
+        // Number of students cannot be more than number of packets
+        if (n < m) {
+            return -1;
+        }
+
+        // Initialize the minimum difference as a large value
+        long minDiff = Long.MAX_VALUE;
+
+        // Find the minimum difference
+        for (int i = 0; i + m - 1 < n; i++) {
+            long diff = a.get(i + m - 1) - a.get(i);
+            if (diff < minDiff) {
+                minDiff = diff;
+            }
+        }
+
+        return minDiff;
+    }
+}
---
+++ longest-common-prefix.java
@@ -0,0 +1,21 @@
+class Solution {
+      public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        int n = strs.length;
+
+        for (int i = 1; i < n; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                    if (prefix.isEmpty()) {
+                        return "";
+                    }
+            }
+        }
+        return prefix;
+    }
+
+}
